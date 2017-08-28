72f044dafa201606dc85722d1390818aebd53062
jam cavarec <jb.cavarec@orange.fr>
2017-08-28 12:14:22 +0200

    async+autres fix

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var async = require('async');
+var im = require('imagemagick');
 var express = require('express');
 var Session = require('express-session');
 
@@ -36,7 +38,7 @@ http.createServer(function (req, res) {
       res.end();
 }).listen(3020);
 
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' })
 app.use(morgan('combined', {stream: accessLogStream}))
 
 accessLogStream.write("DEMARRAGE SERVER :"+new Date());
@@ -86,6 +88,7 @@ io.on("connection",function(socket)
   {   
     
     socket.on("enregistrer",function(sockName,fnc) {
+	    console.log("Enregistre");
 	        socket.join(sockName);
       		caissesSockets[sockName] = { "socket": socket.id } 
       		 Controller.getIms(sockName,function(ret) { 
@@ -97,13 +100,14 @@ io.on("connection",function(socket)
     socket.on("disconnect",function() { 
 	    console.log ("client "+ socket.id+" deconnecté") ;
 	    for (var key in caissesSockets ) {
-	       console.log(key); 
+	       console.log(caissesSockets[key]); 
+	       if ( caissesSockets[key] == socket.id ) { caissesSockets[key] = null }
 	    }
     })
 
     /* 
-    Reception de ticket par socket (ticket envoyé par curl par kerawen mais possibilité par socket
-    socket.on("ticket:"+socket.handshake.query.url_longue+":"+socket.handshake.query.magasin+":"+socket.handshake.query.caisse,function(ticket) {
+      Reception de ticket par socket (ticket envoyé par curl par kerawen mais possibilité par socket
+      socket.on("ticket:"+socket.handshake.query.url_longue+":"+socket.handshake.query.magasin+":"+socket.handshake.query.caisse,function(ticket) {
           var to_caisse = socket.handshake.query.url_longue+socket.handshake.query.magasin+socket.handshake.query.caisse ;
           if (caissesSockets[to_caisse]) {
             //var id = caissesSockets[to_caisse].socket ;
@@ -111,7 +115,7 @@ io.on("connection",function(socket)
           } else {
             socket.emit("caisse:out");
           }
-    })
+      })
     */
 
     socket.on("affiche_pub",function() {
@@ -125,15 +129,20 @@ io.on("connection",function(socket)
 // Afficheur de caisse
 app.get("/mag/:url_longue",function(req,res) {
   res.render('index', { title: 'CAISSE KERAWEN' ,  url_longue: req.params.url_longue  });
-})
+});
+
 
 // reception de ticket par HTTP (POST)
 app.post("/ticket",function(req,res) {
-	console.log("TEMOIN:",req.body.url_longue);
 	var to_c = req.body.url_longue ;
-	var id = caissesSockets[to_c].socket;
-        io.sockets.connected[id].emit("affiche_ticket",req.body.ticket);
-	res.end('ok');
+	if (caissesSockets[to_c] != null ) {
+		// var id = caissesSockets[to_c].socket;
+       		// io.sockets.connected[id].emit("affiche_ticket",req.body.ticket);
+       		io.sockets.in(to_c).emit("affiche_ticket",req.body.ticket);
+		res.end('ok');
+	} else {
+		console.log("Brancher afficheur");
+	}
 })
 
 
@@ -156,9 +165,53 @@ var makePath = function(path) {
   }
 }
 
+app.get('/screenDecoUrls/:shop/:cashdrawer',function(req,res) {
+
+
+    var url = req.protocol + "://" + req.get('host');
+    var shop = req.params.shop ;
+    var cashdrawer = req.params.cashdrawer ;
+
+    var path_url = url + "/uploads/" + shop + "/" + cashdrawer +"/"  ;
+
+    var urls = [] ;
+    var nb=0;
+    var path1 = __dirname + '/public/uploads/'+shop+'/'+cashdrawer+'/';
+     
+    async.each(['pub','thumb_pub','logo','thumb_logo'], 
+        function(filePathPartial, callback) {
+            nb++;
+            filePath = path1+filePathPartial+"_"+shop+".jpg";
+            fs.access(filePath, function(err) {
+                                 var obj = {};
+                                 if (!err) { 
+                                    obj['url_'+filePathPartial] = path_url + filePathPartial+"_"+shop+".jpg" ;
+                                    urls.push(obj);
+                                 } 
+                                 else 
+                                 {
+                                    obj['url_'+filePathPartial] = 'holder.js/400x700?text='+filePathPartial ;
+                                    urls.push(obj);
+                                 }
+            callback();
+        });
+      }, function() {
+           var ret_urls = "{";
+           urls.map(function(x,i){
+             //console.log(Object.keys(x)[0],":",x[Object.keys(x)[0]]);
+            if ( i+1 < nb ) { suite = "," } else { suite = "}" } 
+             ret_urls+= '"'+Object.keys(x)[0]+'":"'+x[Object.keys(x)[0]] +'"'+ suite ;
+             
+             //if (nb < 4) 
+           });
+           console.log(ret_urls);
+           res.end(ret_urls);
+    });
+    
+});
 
 app.post('/upload/', function (req, res) {
-    var fields = []
+    var fields = [];
     var form = new formidable.IncomingForm();
 
     form.on('field',function(field,value){
@@ -170,32 +223,57 @@ app.post('/upload/', function (req, res) {
     });
 
     form.on('progress', function(bytesReceived, bytesExpected) {
-         console.log('progress')
+         // console.log('progress')
     });
 
     form.on('file', function (name, file) {
-        console.log('Uploaded ' + file.name); 
+         
+        var magasin = fields['magasin'];
+        var caisse = fields['caisse'];
+        var nomfichier = fields['nomfichier'];
     
-        fs.ensureDir('public/uploads/'+fields['magasin']+'/'+fields['caisse']+'/',function(){
+        fs.ensureDir('public/uploads/'+magasin+'/'+caisse+'/',function(){
           var source = fs.createReadStream(__dirname + '/uploads/' + file.name);
-          var dest =  fs.createWriteStream(__dirname + '/public/uploads/' + fields['magasin'] + '/' + fields['caisse'] + '/' + fields['nomfichier']);
+          var dest =  fs.createWriteStream(__dirname + '/public/uploads/' + magasin + '/' + caisse + '/' + nomfichier );
 
           source.pipe(dest);
-          source.on('end', function() { console.log('copied'); });
+
+          source.on('end', function() { 
+		        console.log('-> fichier chargé'); 
+
+            		im.identify(__dirname + '/public/uploads/' + magasin + '/' + caisse + '/' + nomfichier ,function(err,features){
+              		if (err) throw err;
+              			// console.log(features);
+            		})
+
+	          	im.resize({
+		             srcPath: __dirname + '/public/uploads/' + magasin + '/' + caisse + '/' + nomfichier ,
+		             dstPath: __dirname + '/public/uploads/' + magasin + '/' + caisse + '/thumb_'+nomfichier ,
+		             width: 256 }, function(err,stdout,stderr) {
+			               if (err) throw err;
+                     // la reponse se fait à la fin du resize
+                     console.log("-> miniature créée");
+                     res.end('{ "magasin": "'+magasin+'", "caisse": "'+caisse+'", "nomfichier": "thumb_'+ nomfichier +'" }');
+		             });
+	         });
+
           source.on('error', function(err) { console.log('error'); });
+        
         })
     });
 
     form.parse(req, function(err, fields, files) {
       res.writeHead(200, {'content-type': 'text/plain'});
-      //res.write('received upload:\n\n');
-      //res.end(util.inspect({fields: fields}));    
-      var magasin = fields['magasin'];
-      var caisse = fields['caisse'];
-      var nomfichier = fields['nomfichier'];
-      res.end('{ "magasin": "'+magasin+'", "caisse": "'+caisse+'", "nomfichier": "'+ nomfichier +'" }');
+      // res.write('received upload:\n\n');
+      // res.end(util.inspect({fields: fields}));    
+      /*
+          var magasin = fields['magasin'];
+          var caisse = fields['caisse'];
+          var nomfichier = fields['nomfichier'];
+          res.end('{ "magasin": "'+magasin+'", "caisse": "'+caisse+'", "nomfichier": "thumb_'+ nomfichier +'" }');
+      */
+
     });
 });
 
 server.listen(3030);
-
